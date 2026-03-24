3e22e347ad536891eff4fef9cce1b1f7bb8b3176
suryanshcode2601 <suryanshwork2601@gmail.com>
2026-03-25 03:18:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,11 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "https://sankalp-govlink-lh21.vercel.app"  
-    ],
+    allow_origins=[*],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
