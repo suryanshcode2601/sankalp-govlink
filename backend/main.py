9d6119653a066031e775c09741e880c90276564f
suryanshcode2601 <suryanshwork2601@gmail.com>
2026-03-25 02:26:34 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://sankalp-govlink.vercel.app/"
 ],
     allow_credentials=True,
     allow_methods=["*"],
