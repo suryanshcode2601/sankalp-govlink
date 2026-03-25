3f6e59d2ca238df0cdd7f19d25d9a4a5cc8327fb
suryanshcode2601 <suryanshwork2601@gmail.com>
2026-03-26 02:06:38 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# db_url = "postgresql://postgres:postgresaks@localhost:5432/govlink"
+
 db_url = os.getenv("DATABASE_URL")
 
 engine = create_engine(db_url)
